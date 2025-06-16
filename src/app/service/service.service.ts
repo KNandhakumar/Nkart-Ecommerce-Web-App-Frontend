f6aaba33817c5adfd11e8f8d20492a729b6dc44a
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-16 21:40:43 +0530

    add : api url

--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class ServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  
+  // api url
+  private apiUrl = 'http://localhost:8080/api/products';
 }
