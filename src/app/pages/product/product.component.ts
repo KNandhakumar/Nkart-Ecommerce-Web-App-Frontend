ce75adb40d092da84efb1a0cc8790dc439230da5
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-16 22:27:53 +0530

    component Ts : get all products to show on frontend

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../../service/service.service';
+import { Product } from '../../model/Product.model';
 
 @Component({
   selector: 'app-product',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
 
+  constructor(private productService:ServiceService) {}
+
+  // get all products to show
+  products:Product[] = [];
+  ngOnInit(): void {
+    this.getAllProducts();
+  };
+  
+  getAllProducts(){
+    this.productService.getAllProducts().subscribe((data) => {
+      this.products = data;
+    })
+  };
+  
 }
