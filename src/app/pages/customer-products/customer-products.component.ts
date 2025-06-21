5df5e8d68082a31ad22d3cbe7a622118d5d57a5f
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-21 14:08:30 +0530

    component : customer-products file generated

---
+++ src/app/pages/customer-products/customer-products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-products',
+  imports: [],
+  templateUrl: './customer-products.component.html',
+  styleUrl: './customer-products.component.css'
+})
+export class CustomerProductsComponent {
+
+}
