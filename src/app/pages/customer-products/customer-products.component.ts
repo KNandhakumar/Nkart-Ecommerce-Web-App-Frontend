67c2d9df07476aad27e9a67e14162349c5f8d741
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-21 14:20:04 +0530

    customer-products : get all products method complete

--- src/app/pages/customer-products/customer-products.component.ts
+++ src/app/pages/customer-products/customer-products.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../../service/service.service';
+import { Product } from '../../model/Product.model';
 
 @Component({
   selector: 'app-customer-products',
@@ -6,6 +8,15 @@ import { Component } from '@angular/core';
   templateUrl: './customer-products.component.html',
   styleUrl: './customer-products.component.css'
 })
-export class CustomerProductsComponent {
+export class CustomerProductsComponent implements OnInit{
+  // import service
+  constructor(private productService:ServiceService){}
 
+  // get all products
+  products:Product[] = [];
+  ngOnInit(): void {
+    this.productService.getAllProducts().subscribe((data) => {
+      this.products = data;
+    });
+  };
 }
