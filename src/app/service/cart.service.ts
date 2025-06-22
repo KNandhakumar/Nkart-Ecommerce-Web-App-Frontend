939d6cdc7b60b797559354f360717a7afb54c464
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-22 21:18:25 +0530

    cart service : service complete to save,get,remove,add

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../model/Product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,28 @@ import { Injectable } from '@angular/core';
 export class CartService {
 
   constructor() { }
+  // cart
+  cart:Product[] = [];
+  addToCart(product:Product){
+    this.cart.push(product);
+    this.saveCart();
+  };
+
+  removeFromCart(index:number){
+    this.cart.splice(index,1);
+    this.saveCart();
+  };
+
+  getCart():Product[]{
+    this.cart = JSON.parse(localStorage.getItem('cart') || '[]');
+    return this.cart;
+  };
+
+  getTotal():number{
+    return this.cart.reduce((total,item) => total + item.price, 0);
+  };
+
+  saveCart(){
+    localStorage.setItem('cart',JSON.stringify(this.cart));
+  };
 }
