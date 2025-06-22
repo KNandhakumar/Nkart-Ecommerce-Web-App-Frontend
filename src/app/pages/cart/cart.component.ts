dd69307b4a2a5e8656efde5eb327b963ece97e4a
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-22 22:01:23 +0530

    cart component : get product and total price from local storage

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../model/Product.model';
+import { CartService } from '../../service/cart.service';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent {
+export class CartComponent implements OnInit{
 
+  constructor(private cartService:CartService){}
+
+  // cart datas
+  cart:Product[] = [];
+  total:number = 0;
+
+  // get product data from local storage
+  ngOnInit(): void {
+    this.cart = this.cartService.getCart(); // get carted product from local storage
+    this.total = this.cartService.getTotal(); // get total price from carted products
+  }
 }
