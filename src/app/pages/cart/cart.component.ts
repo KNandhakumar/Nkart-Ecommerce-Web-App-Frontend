3544f29394898e3d56e1520d63a27653a2be2909
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-22 22:05:44 +0530

    cart component : remove from cart product

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -10,6 +10,11 @@ import { CartService } from '../../service/cart.service';
   styleUrl: './cart.component.css'
 })
 export class CartComponent implements OnInit{
+  // we can
+  // * add product to cart
+  // * view cart items
+  // * remove item
+  // * see total price
 
   constructor(private cartService:CartService){}
 
@@ -22,4 +27,11 @@ export class CartComponent implements OnInit{
     this.cart = this.cartService.getCart(); // get carted product from local storage
     this.total = this.cartService.getTotal(); // get total price from carted products
   }
+
+  remove(index:number){
+    // product remove from cart
+    this.cartService.removeFromCart(index);
+    this.cart = this.cartService.getCart();
+    this.total = this.cartService.getTotal();
+  };
 }
