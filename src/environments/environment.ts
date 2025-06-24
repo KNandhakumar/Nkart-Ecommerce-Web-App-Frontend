33084eae578ac403a623102f7a7bd86c4bb59fb5
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-24 18:55:37 +0530

    firebase : created environment folder and added firebase config to tal to angular

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCj-ialXlICHn_aGgzP2FQIrH3_2HkxY-c",
+  authDomain: "nkart-auth.firebaseapp.com",
+  projectId: "nkart-auth",
+  storageBucket: "nkart-auth.firebasestorage.app",
+  messagingSenderId: "865142237747",
+  appId: "1:865142237747:web:08f7def06d394adc215146"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
