18b05b13c30107417037e08eebd5f68bd965844f
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-25 16:22:40 +0530

    auth guard and router : give access to users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
-import { Routes } from '@angular/router';
+import { Routes, CanActivateFn } from '@angular/router';
 import { ProductComponent } from './pages/product/product.component';
 import { CustomerProductsComponent } from './pages/customer-products/customer-products.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
+import { authGuard } from './guards/auth.guard';
 
 
 export const routes: Routes = [
     {path: '', component: ProductComponent},
-    {path: 'shop', component: CustomerProductsComponent},
-    {path: 'product/:id', component: ProductDetailsComponent},
-    {path: 'cart', component: CartComponent},
+    {path: 'shop', component: CustomerProductsComponent,},
+    {path: 'product/:id', component: ProductDetailsComponent, canActivate: [authGuard]},
+    {path: 'cart', component: CartComponent, canActivate: [authGuard]},
     {path: 'signup', component: SignupComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+
 ];
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { getAuth } from 'firebase/auth';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router);
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  if(user){
+    return true; // allow access
+  }
+  else {
+    alert('Please login first!');
+    router.navigate(['/login']);
+    return false; // block access
+  }
 };
