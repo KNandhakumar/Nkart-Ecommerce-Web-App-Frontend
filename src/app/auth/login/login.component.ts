562140986fcf836c0cac1416bd651fc0b4f0e991
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-25 11:49:46 +0530

    auth login : completed login with firebase

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor(private auth:Auth, private router:Router){}
+
+  email = '';
+  password = '';
+
+  onLogin(){
+    signInWithEmailAndPassword(this.auth,this.email,this.password)
+    .then(() => {
+      alert('Login successful!');
+      this.router.navigate(['/shop']); // redirect to shop page
+    }).catch(err => alert('something went wrong' + " " + err.message));
+  };
 }
