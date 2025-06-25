5325120007cfb2cebb3b87bde885781659f03037
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-25 16:00:32 +0530

    customer-products : added logout button for users can logout

--- src/app/pages/customer-products/customer-products.component.ts
+++ src/app/pages/customer-products/customer-products.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../../service/service.service';
 import { Product } from '../../model/Product.model';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Auth } from '@angular/fire/auth';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
 
 @Component({
   selector: 'app-customer-products',
@@ -12,13 +14,31 @@ import { RouterLink } from '@angular/router';
 })
 export class CustomerProductsComponent implements OnInit{
   // import service
-  constructor(private productService:ServiceService){}
+  constructor(private productService:ServiceService,
+    private auth:Auth,
+    private router:Router
+  ){}
+
+  // logout logic
+  isLoggedIn = false;
 
   // get all products
   products:Product[] = [];
   ngOnInit(): void {
     this.productService.getAllProducts().subscribe((data) => {
       this.products = data;
+    });
+
+    // logout
+    onAuthStateChanged(this.auth,(user) => {
+      this.isLoggedIn = !!user;
+    });
+  };
+
+  logout(){
+    signOut(this.auth).then(() => {
+      alert('Logged out!');
+      this.router.navigate(['/login']);
     });
   };
 }
